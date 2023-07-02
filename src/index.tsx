5f3a7ff0640e59d286042f6de69b6fd9a8dd8406
Pavel <pavelrevin12@gmail.com>
2023-07-02 17:54:13 +0600

    добавлены стили

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import {render} from "react-dom";
 import "./global.scss"
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
+import "./styles/index.scss"
 render(
     <BrowserRouter>
         <App/>
