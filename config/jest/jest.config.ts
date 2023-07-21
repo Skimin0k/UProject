62e75267aff4f6e81f71af425bed4a3945618a6a
Pavel <pavelrevin12@gmail.com>
2023-07-21 07:05:33 +0600

    fix unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -155,7 +155,7 @@ export default {
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/config/jest/__mocks__/FileMock.tsx',
+        '<rootDir>config/jest/__mocks__/FileMock.tsx',
     '\\.(scss|less)$': 'identity-obj-proxy',
   },
 
