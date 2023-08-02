26d323a8fdcbc9e6a61827c8e855cf386d251057
Pavel <pavelrevin12@gmail.com>
2023-08-02 07:35:48 +0600

    Изменил размещение конфигов eslint и stylelint

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -1,16 +1,43 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'amd': true,
-        'node': true,
-        'es2021': true
-    },
+    'plugins': [
+        '@typescript-eslint',
+        'react',
+        'i18next',
+        'simple-import-sort'
+    ],
     'extends': [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
-        'plugin:i18next/recommended'
+        'plugin:i18next/recommended',
+        'plugin:react-hooks/recommended'
     ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'import/prefer-default-export': 'off',
+        '@typescript-eslint/no-unused-vars': [1],
+        'react/react-in-jsx-scope': 'off',
+        'i18next/no-literal-string': [2, {markupOnly: true}],
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'error',
+    },
     'overrides': [
         {
             'env': {
@@ -46,43 +73,25 @@ module.exports = {
                     }
                 ]
             }
+        },
+        {
+            'files': ['src/examples/**'],
+            'rules': {
+                'i18next/no-literal-string': 'off'
+            }
         }
 
     ],
     'ignorePatterns': ['*.test.*'],
+    'env': {
+        'browser': true,
+        'amd': true,
+        'node': true,
+        'es2021': true
+    },
     'parser': '@typescript-eslint/parser',
     'parserOptions': {
         'ecmaVersion': 'latest',
         'sourceType': 'module'
     },
-    'plugins': [
-        '@typescript-eslint',
-        'react',
-        'i18next',
-        'simple-import-sort'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'import/prefer-default-export': 'off',
-        '@typescript-eslint/no-unused-vars': [1],
-        'react/react-in-jsx-scope': 'off',
-        'i18next/no-literal-string': [2, {markupOnly: true}],
-        'simple-import-sort/imports': 'error',
-        'simple-import-sort/exports': 'error'
-    }
 }
